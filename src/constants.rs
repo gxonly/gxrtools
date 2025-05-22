dcb73904eedd0bde2e6033183493b99d3e790f15
withmeT <withme00@163.com>
2025-05-22 08:58:46 +0800

    优化为yaml传入命令

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,29 @@
 // src/constants.rs
 
+// pub fn default_ssh_commands() -> Vec<String> {
+//     vec![
+//         "df -h".to_string(),
+//         "uptime".to_string(),
+//         "whoami".to_string(),
+//     ]
+// }
+
+
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Debug, Deserialize)]
+struct CommandConfig {
+    ssh_commands: Vec<String>,
+}
+
 pub fn default_ssh_commands() -> Vec<String> {
-    vec![
-        "df -h".to_string(),
-        "uptime".to_string(),
-        "whoami".to_string(),
-    ]
+    let path: &str = "cmd.yaml";
+    let content = fs::read_to_string(path)
+        .expect("Failed to read YAML file");
+
+    let config: CommandConfig = serde_yaml::from_str(&content)
+        .expect("Failed to parse YAML file");
+
+    config.ssh_commands
 }
