b756683d2d1d68d7077228276df73b6d5fd2ef3c
withmeT <withme00@163.com>
2025-05-26 15:47:56 +0800

    杂项

--- windows.ps1
+++ windows.ps1
@@ -134,7 +134,7 @@ $commandResult = $combinedResult | ConvertTo-Json -Depth 3 -Compress
 
 
 # 接口地址
-$url = "http://192.168.101.97:3000/report"
+$url = "http://192.168.100.1:3000/report"
 
 # 构造请求头（如需授权，可以加 Authorization）
 $headers = @{
